9bf18b0fb742ca73a2093d92c95411403cc505ff
Fernanda Canêjo <fernanda.canejo@gmail.com>
2025-06-18 16:49:30 -0300

    create project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("botaoenviar").addEventListener("click", validaFormulario);
+
+function validaFormulario () {
+      if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" &&  document.getElementById("telefone").value != "") {
+      alert("Tudo certo!  Você receberá as novidades por email.")
+   }else {
+      alert("Por favor, preencha todos os campos obrigatórios.")
+   }
+}
